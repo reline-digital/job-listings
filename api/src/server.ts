4df5b4b3c1a144e7b844038270d1f0334c5b3ee0
Haris Shah <harisshah385@gmail.com>
2023-04-16 23:10:31 +0500

    Add basic routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,19 @@ import express from 'express'
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log('server running...')
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!' })
+})
+// routes that doesnt need auth
+app.get('/api/jobs', (req, res) => {
+  res.status(200).json({ jobs: 'All jobs' })
+})
+app.get('/api/user', (req, res) => {
+  res.status(200).json({ user: 'user signup' })
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
 })
