d45c0e6b980e8433536671ccbb06c077ef1a85dd
Turbobot <turbobot@vercel.com>
2024-01-27 20:36:24 +0500

    feat(create-turbo): apply official-starter transform

--- client/components/Button.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Button() {
-  return <button className='border text-red-300 text-5xl'>Click</button>
-}
--- client/components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export const Navbar = () => {
-  return (
-    <nav>
-      <h2>Navbar here</h2>
-    </nav>
-  )
-}
