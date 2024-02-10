df014ee89da5d5b555092c9e074643b43ea8b4d4
Haris Shah <harisshah385@gmail.com>
2024-02-10 14:33:33 +0500

    setup: add basic setup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,8 @@
-import { log } from "@repo/logger";
-import { createServer } from "./server";
+import { log } from '@repo/logger'
+import { createServer } from './server'
 
-const port = process.env.PORT || 5001;
-const server = createServer();
-
+const port = process.env.PORT || 5001
+const server = createServer()
 server.listen(port, () => {
-  log(`api running on ${port}`);
-});
+  log(`api running on ${port}`)
+})
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,22 +1,25 @@
-import { json, urlencoded } from "body-parser";
-import express, { type Express } from "express";
-import morgan from "morgan";
-import cors from "cors";
+import { json, urlencoded } from 'body-parser'
+import express, { type Express } from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import { config } from 'dotenv'
+
+config()
 
 export const createServer = (): Express => {
-  const app = express();
+  const app = express()
   app
-    .disable("x-powered-by")
-    .use(morgan("dev"))
+    .disable('x-powered-by')
+    .use(morgan('dev'))
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
+    .get('/message/:name', (req, res) => {
+      return res.json({ message: `hello ${req.params.name}` })
     })
-    .get("/status", (_, res) => {
-      return res.json({ ok: true });
-    });
+    .get('/status', (_, res) => {
+      return res.json({ ok: true })
+    })
 
-  return app;
-};
+  return app
+}
